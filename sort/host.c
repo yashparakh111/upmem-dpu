546ad6191bc564425a50008937172e8229c51505
Arjun Ramesh <arjunramesh@utexas.edu>
2021-05-09 18:06:50 -0500

    Added chunk sorting verification

--- sort/host.c
+++ sort/host.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "config.h"
 
@@ -13,6 +14,7 @@
 #endif
 
 #define ARRAY_SIZE 64
+#define CHUNK_SIZE 16
 
 int comp(const void *elem1, const void *elem2)
 {
@@ -41,6 +43,25 @@ void print_arr(char *name, uint32_t *arr, int size)
         printf("%u ", arr[i]);
     }
     printf("\n\n");
+}
+
+bool verify_chunks(uint32_t* dpu_arr, uint32_t* cpu_arr, int size, int chunk_size) {
+  uint32_t golden_chunk[chunk_size];
+  uint32_t iters = size / chunk_size;
+  for (int i = 0; i < iters; i++) {
+    uint32_t base = i*chunk_size;
+    memcpy(golden_chunk, &cpu_arr[base], sizeof(uint32_t) * chunk_size);
+    qsort(golden_chunk, chunk_size, sizeof(uint32_t), comp);
+    
+    for (int j = 0; j < chunk_size; j++) {
+      if (golden_chunk[j] != dpu_arr[base+j]) {
+        printf("Error at chunk: %d, idx: %d. Expected %d, got %d", i, j,\
+          golden_chunk[j], dpu_arr[base+j]);
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 void launch_dpu(uint32_t *in_arr, uint32_t *out_arr)
@@ -100,5 +121,9 @@ int main(void)
     print_arr("In", in_arr, ARRAY_SIZE);
     print_arr("Out DPU", out_arr, ARRAY_SIZE);
     print_arr("Out CPU", out_arr_golden, ARRAY_SIZE);
+    if (verify_chunks(out_arr, in_arr, ARRAY_SIZE, CHUNK_SIZE))
+      printf("Successful chunk sorting\n");
+    else
+      printf("Failure in chunk sorting\n");
     return 0;
 }
