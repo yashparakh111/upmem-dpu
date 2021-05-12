e99ca59c04a627aa2e642902b5fda68f774b3859
srafu <saraf.arul@utexas.edu>
2021-05-12 01:08:18 -0500

    kinda working

--- sort_cpp/src/sort.cpp
+++ sort_cpp/src/sort.cpp
@@ -4,8 +4,10 @@
 
 #include <stdio.h>
 
-#include <dpu.h>
-#include <dpu_log.h>
+//#include <dpu.h>
+//#include <dpu_log.h>
+
+#include <dpu>
 
 #include "config.h"
 
@@ -15,6 +17,8 @@
 
 uint32_t execution_cycles;
 double execution_time;
+
+using namespace dpu;
 
 void print_arr_2(char *name, uint32_t *arr, int size)
 {
@@ -34,52 +38,59 @@ void sort_dpu(uint32_t size, uint32_t *in_arr, uint32_t *out_arr)
 
     num_dpu_needed = (size + (BUFFER_SIZE - 1)) / BUFFER_SIZE;
     uint32_t dpu_alloc_ct = (num_dpu_needed > DPU_ALLOCATE_ALL) ? DPU_ALLOCATE_ALL : num_dpu_needed;
-    DPU_ASSERT(dpu_alloc(dpu_alloc_ct, NULL, &set));
-    DPU_ASSERT(dpu_get_nr_dpus(set, &num_dpus));
+
+    //DPU_ASSERT(dpu_alloc(dpu_alloc_ct, NULL, &set));
+    //DPU_ASSERT(dpu_get_nr_dpus(set, &num_dpus));
+    auto system = DpuSet::allocate(dpu_alloc_ct, "backend=simulator");
+    auto dpus = system.dpus();
+
+    num_dpus = dpus.size();
 
     int dpu_loop_cnt = (num_dpu_needed + (num_dpus - 1)) / num_dpus;
 
-    //printf("Number of DPUs needed: %d\n", num_dpu_needed);
-    //printf("Using %u dpu(s)\n", num_dpus);
-    //printf("Using %u dpu loop(s)\n", dpu_loop_cnt);
+    printf("Number of DPUs needed: %d\n", num_dpu_needed);
+    printf("Using %u dpu(s)\n", num_dpus);
+    printf("Using %u dpu loop(s)\n", dpu_loop_cnt);
 
     // load the binary into the dpu
-    DPU_ASSERT(dpu_load(set, DPU_BINARY, NULL));
+    //DPU_ASSERT(dpu_load(set, DPU_BINARY, NULL));
+    for(uint32_t each_dpu = 0; each_dpu < dpus.size(); each_dpu++){
+        dpus[each_dpu]->load(DPU_BINARY);
+    }
 
-    // load data into the dpu
+    printf("Here0\n");
+
+        // load data into the dpu
     // using xfer lets us maximize transfer bandwidth
     for (int dpu_loop = 0; dpu_loop < dpu_loop_cnt; dpu_loop++)
     {
+    
+        std::vector<std::vector<uint32_t>> in_arr_wrp; 
+        std::vector<std::vector<uint32_t>> out_arr_wrp(num_dpus, std::vector<uint32_t>(num_dpus, 0));
+
+        for(uint32_t each_dpu = 0; each_dpu < dpus.size(); each_dpu++){
+            uint32_t offset = (dpu_loop * num_dpus + each_dpu) * BUFFER_SIZE;
+            in_arr_wrp.push_back(std::vector<uint32_t>(in_arr + offset, in_arr + offset + BUFFER_SIZE));
+        }
+
+        printf("Here1\n");
+
         //printf("loop: %d\n", dpu_loop);
         //may not need all dpus for last loop -- TODO
         uint32_t num_dpu_needed_iter = ((dpu_loop + 1) == dpu_loop_cnt) ? (dpu_alloc_ct - (num_dpu_needed % num_dpus)) : num_dpus;
         //printf("Need %d DPUs for cycle %d\n", num_dpu_needed_iter, dpu_loop);
 
-        DPU_FOREACH(set, dpu, each_dpu)
-        {
-            //if (each_dpu < num_dpu_needed_iter)
-            DPU_ASSERT(dpu_prepare_xfer(dpu, &in_arr[(dpu_loop * num_dpus + each_dpu) * BUFFER_SIZE]));
+        for(int each_dpu = 0; each_dpu < dpus.size(); each_dpu++){
+            dpus[each_dpu]->async().copy("mem", 0, in_arr_wrp, BUFFER_SIZE*sizeof(uint32_t));
+            dpus[each_dpu]->async().exec();
+            dpus[each_dpu]->async().copy(out_arr_wrp, BUFFER_SIZE*sizeof(uint32_t), "mem", 0);
+            dpus[each_dpu]->async().sync();
         }
-        DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "mem", 0, sizeof(uint32_t) * BUFFER_SIZE, DPU_XFER_ASYNC));
 
-        // start executing program
-        DPU_ASSERT(dpu_launch(set, DPU_ASYNCHRONOUS));
+        printf("Here2\n");
+        //system.sync();
 
-        // loops through each DPU in SET
-        DPU_FOREACH(set, dpu, each_dpu)
-        {
-            // get data from the dpu
-            //if (each_dpu < num_dpu_needed_iter)
-            DPU_ASSERT(dpu_prepare_xfer(dpu, &out_arr[(dpu_loop * num_dpus + each_dpu) * BUFFER_SIZE]));
-            // DPU_ASSERT(dpu_log_read(dpu, stdout));
-        }
-        DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_FROM_DPU, "mem", 0, sizeof(uint32_t) * BUFFER_SIZE, DPU_XFER_ASYNC));
-
-        // sync barrier for all dpus
-
-        DPU_ASSERT(dpu_sync(set));
-
-        DPU_FOREACH(set, dpu, each_dpu)
+        /*DPU_FOREACH(set, dpu, each_dpu)
         {
             // get data from the dpu
             //if (each_dpu < num_dpu_needed_iter)
@@ -87,10 +98,11 @@ void sort_dpu(uint32_t size, uint32_t *in_arr, uint32_t *out_arr)
             DPU_ASSERT(dpu_copy_from(dpu, "execution_cycles", 0, &execution_cycles, sizeof(uint32_t)));
             DPU_ASSERT(dpu_copy_from(dpu, "execution_time", 0, &execution_time, sizeof(double)));
             // }
-        }
+        }*/
+
     }
     // release the allocated dpus
-    DPU_ASSERT(dpu_free(set));
+    //DPU_ASSERT(dpu_free(set));
 }
 
 void merge_blocks(int i, int j, uint32_t *a, uint32_t *aux)
