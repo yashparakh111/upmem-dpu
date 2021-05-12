fb8f95ab2eb8c5e813977fc2b2abd06d93333589
Yash Parakh <yash_parakh111@upmemcloud7.cloud.upmem.com>
2021-05-13 00:54:47 +0200

    Hardware run results

--- sort/multidpu_test_perf.sh
+++ sort/multidpu_test_perf.sh
@@ -7,9 +7,9 @@ CPPOUT=$OUTFILE.cpp.out
 rm -f $COUT
 rm -f $CPPOUT 
 
-for arr_log in {16..30}
+for arr_log in {16..25}
 do
-    for buf_log in {12..24}
+    for buf_log in {12..20}
     do
         for cache_log in {8..8}
         do
@@ -18,7 +18,9 @@ do
                     CACHE_SIZE=$((1<<$cache_log)) || exit $?
 
                 #$DIR/host_c $((1<<$arr_log)) 0 >> $COUT
-                $DIR/host_cpp $((1<<$arr_log)) 0 >> $CPPOUT
+                dpu-profiling memory-transfer -- $DIR/host_cpp $((1<<$arr_log)) 0 >> perf/mem/$arr_log.$buf_log.$cache_log.out
+                # dpu-profiling functions -f "APPLICATION_FUNCTIONS" -d "DPU_HOST_API_FUNCTIONS" -- $DIR/host_cpp $((1<<$arr_log)) 0 >> perf/func/$arr_log.$buf_log.$cache_log.out
+                # dpu-profiling functions -o perf/traces/$arr_log.$buf_log.$cache_log.json -A -- $DIR/host_cpp $((1<<$arr_log)) 0 
             fi
         done
     done
